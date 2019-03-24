c79aea8f89ce6ed547618da5ef6c696ed3b0c034
Vitaly Kashtalyan <vitaly.kashtalyan@gmail.com>
2019-03-24 14:21:57 +0300

    Implemented gin service

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
-# Base build image
 FROM golang:1.12
-WORKDIR /go/src/bitbucket.org/wunzi/woinder-backend
 ENV GO111MODULE=on
+WORKDIR /app
 COPY go.mod .
 COPY go.sum .
 RUN go mod download
 COPY . .
-#RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./main.go
-#CMD ["./main"]
-RUN go build ./main.go
-#CMD ["./main"]
-#RUN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go install -a -tags netgo -ldflags '-w -extldflags "-static"' ./cmd/weaviate-server
-#RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o /go/src/bitbucket.org/wunzi/woinder-backend
-#CMD ["./main"]
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./main.go
+CMD ["./main"]
 EXPOSE 8080
--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"net/http"
+)
+
+type httpCode struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		db, err := gorm.Open("postgres", "host=pgd_db port=5432 user=db_user dbname=db_name password=mypassword sslmode=disable")
+		if err != nil {
+			resCode(c, http.StatusInternalServerError, err)
+		} else {
+			resCode(c, http.StatusOK, db)
+		}
+		defer db.Close()
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func resCode(ctx *gin.Context, code int, data ...interface{}) {
+	er := httpCode{
+		Status:  code,
+		Message: http.StatusText(code),
+		Data:    data,
+	}
+	ctx.JSON(http.StatusOK, er)
 }
